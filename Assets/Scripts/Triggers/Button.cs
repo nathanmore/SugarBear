cceb3a0cba84a25743b2834cbe3b1b4babb7b723
coleyoung02 <coleyoung741@gmail.com>
2023-04-22 15:37:27 -0700

    one time use button

--- Assets/Scripts/Triggers/Button.cs
+++ Assets/Scripts/Triggers/Button.cs
@@ -2,11 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Button : MonoBehaviour
+public class Button : OneTimeButton
 {
-    [SerializeField] private GameObject unpressed;
-    [SerializeField] private GameObject pressed;
-    [SerializeField] private Interactable[] linked;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.layer == LayerMask.NameToLayer("Bear"))
---
+++ Assets/Scripts/Triggers/OneTimeButton.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneTimeButton : MonoBehaviour
+{
+    [SerializeField] protected GameObject unpressed;
+    [SerializeField] protected GameObject pressed;
+    [SerializeField] protected Interactable[] linked;
+    private bool down = false;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!down && collision.gameObject.layer == LayerMask.NameToLayer("Bear"))
+        {
+            unpressed.SetActive(false);
+            pressed.SetActive(true);
+            if (linked != null)
+            {
+                for (int i = 0; i < linked.Length; ++i)
+                {
+                    linked[i].trigger(true);
+                }
+            }
+            else
+            {
+                Debug.Log("null linked object");
+            }
+        }
+    }
+}
