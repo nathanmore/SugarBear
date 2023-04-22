1da46da70f8623f58ac367bbd2d90167cd0480d2
Nathan More <morenate2000@gmail.com>
2023-04-21 23:08:11 -0700

    Got prototype of platform movement with two characters done

---
+++ Assets/Scripts/BeeCharacter.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeeCharacter : PlayerController
+{
+    new void Start()
+    {
+        base.Start();
+    }
+
+    new void Update()
+    {
+        base.Update();
+    }
+}
