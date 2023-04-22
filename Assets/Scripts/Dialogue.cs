75d2683f863f558755cd6f772f745621aafdb0fa
Nathan More <morenate2000@gmail.com>
2023-04-22 13:41:54 -0700

    Added a basic dialogue system

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialogue : MonoBehaviour
+{
+    [Header("Components")]
+    [SerializeField] private GameObject dialogueBox;
+    [SerializeField] private TextMeshProUGUI textDisplay;
+
+    [Header("Dialogue")]
+    [SerializeField] private string[] spokenLines;
+    [SerializeField] private float typeDelay = 0.02f;
+
+    private int index = 0;
+    private string currentLine = "";
+
+    private void Start()
+    {
+        StartDialogue();
+    }
+
+    public void Update()
+    {
+        if (dialogueBox.activeSelf)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (textDisplay.text == currentLine)
+                {
+                    NextLine();
+                }
+                else
+                {
+                    StopAllCoroutines();
+                    textDisplay.text = currentLine;
+                }
+            }
+        }
+    }
+
+    public void StartDialogue()
+    {
+        UpdatePlayerInput(false);
+        dialogueBox.SetActive(true);
+        textDisplay.text = "";
+        currentLine = spokenLines[index];
+        StartCoroutine(Type());
+    }
+
+    public void UpdatePlayerInput(bool newVal)
+    {
+        GameObject[] playerCharacters = GameObject.FindGameObjectsWithTag("Player");
+        foreach (GameObject character in playerCharacters)
+        {
+            if (character != null)
+            {
+                character.GetComponent<IPlayerInput>().InputEnabled = newVal;
+            }
+        }
+    }
+
+    IEnumerator Type()
+    {
+        foreach(char letter in currentLine.ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typeDelay);
+        }
+    }
+
+    public void NextLine()
+    {
+        index++;
+
+        if (index < spokenLines.Length)
+        {
+            textDisplay.text = "";
+            currentLine = spokenLines[index];
+            StartCoroutine(Type());
+        }
+        else
+        {
+            FinishDialogue();
+        }
+    }
+
+    public void FinishDialogue()
+    {
+        textDisplay.text = "";
+        dialogueBox.SetActive(false);
+        UpdatePlayerInput(true);
+        GameObject.Destroy(this);
+    }
+}
