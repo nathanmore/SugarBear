37eee2f351315f49e88bfad954040a597db1a02b
coleyoung02 <coleyoung741@gmail.com>
2023-04-23 15:46:02 -0700

    lever+button art

--- Assets/Scripts/Triggers/Lever.cs
+++ Assets/Scripts/Triggers/Lever.cs
@@ -10,6 +10,7 @@ public class Lever : MonoBehaviour
     [SerializeField] private GameObject unflipped;
     [SerializeField] private GameObject flipped;
     [SerializeField] private Interactable[] linked;
+    [SerializeField] private Animator animator;
     private bool isFlipped;
     private Collider2D col;
 
@@ -19,6 +20,16 @@ public class Lever : MonoBehaviour
         isFlipped = false;
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        animator.SetBool("IsIn", true);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        animator.SetBool("IsIn", false);
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -26,9 +37,10 @@ public class Lever : MonoBehaviour
         {
             if (col.IsTouchingLayers(LayerMask.GetMask("Bees")))
             {
+                animator.SetTrigger("Flip");
                 isFlipped = !isFlipped;
-                unflipped.SetActive(!isFlipped);
-                flipped.SetActive(isFlipped);
+                //unflipped.SetActive(!isFlipped);
+                //flipped.SetActive(isFlipped);
                 if (linked != null)
                 {
                     for (int i = 0; i < linked.Length; ++i)
