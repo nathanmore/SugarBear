fbe8023a5a4c07e1e04b597bde69b911d63b13f2
DragonTamer13 <wynekids@yahoo.com>
2023-04-23 17:24:53 -0700

    End Scene

---
+++ Assets/Scripts/Win_UI.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Win_UI : MonoBehaviour
+{
+    [SerializeField] private GameObject WinUI;
+    
+    void Collision()
+    {
+        
+    }
+
+    
+}
